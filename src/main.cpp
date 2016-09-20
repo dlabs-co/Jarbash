111d3e59c98679ccf38a03559a98208d26b25bd5
lugerh <luige.rh@gmail.com>
2016-09-20 20:27:20 +0200

    Comented main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,17 +33,16 @@ void test()
 int main()
 {	
 	std::ifstream banner("banner", std::ifstream::in);
-	bool b_exists = false;
-	if(banner.good())
-	{
-		b_exists = true;
-	}
 
 	WINDOW *wins[4];
 	PANEL  *panels[4];
 	CDKSCREEN *scr1;
 	int lines = 25, cols = 100, y = 2, x = 4;
-	
+	// cambiar lines y cols por LINES y COLS (verificando si funciona)
+	// recordar verificar las proporciones
+	// #defines para espacios entre ventanas y esas cosas
+	// COLS = es el numero de columnas de caracteres en la pantalla (en horizontal)
+	// LINES = es el numero de lineas que tiene la pantalla (en vertical)
 	initscr();
 	start_color();
 	cbreak();
@@ -54,9 +53,9 @@ int main()
 	init_pair(2, COLOR_BLUE, COLOR_BLUE);
 
 
-	wins[0] = newwin(lines, 100, y, x);
+	wins[0] = newwin(lines, cols, y, x);
 	wins[1] = newwin(lines, cols, y + lines+1, x);
-	wins[2] = newwin((lines*2)+1, 70, y, x + cols+1);
+	wins[2] = newwin((lines*2)+1, round(cols * 0,7), y, x + cols+1);
 	
 	
 	scr1 = initCDKScreen(wins[2]);
@@ -78,9 +77,7 @@ int main()
 		wattron(wins[0], COLOR_PAIR(1));
 		doupdate();
 		attron(A_BOLD);
-		
-		if(b_exists) printBanner(wins[0], banner);
-		
+		if(banner.good()) printBanner(wins[0], banner); // verifica que el banner existe (fichero)
 		wsystem(my_wins[1]);	
 		update_panels();
 		refreshCDKScreen(scr1);
