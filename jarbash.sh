e07a98edbd6e4011c23e3e7433b393247322fbc8
Alex T <alext@ideado.net>
2016-10-18 19:16:28 +0200

    Added jarbash script

---
+++ jarbash.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+
+if test -e /root/Recuerdos/tmp/pidtmp.txt; then
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "           |       |"
+echo "          ==    == |"
+echo "           O  | O "
+echo "              |_   "
+echo "                         "
+echo "              ---   "
+echo "                 |  "
+echo "              ---   "
+echo "                        " 
+echo "                        "
+} >> jarbash.txt
+
+sleep 0.2
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "           |       |"
+echo "          ==    == | "
+echo "           O  | O  "
+echo "              |_   "
+echo "                    "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} >> jarbash.txt
+
+sleep 0.2
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "           |       |"
+echo "          ==    == |"
+echo "           X  | X  "
+echo "              |_   "
+echo "                   "
+echo "              ---   "
+echo "                 |  "
+echo "              ---   "
+echo "                          "
+echo "                          "
+ 
+} > jarbash.txt
+
+sleep 0.2
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "          ==    == |"
+echo "                   |"
+echo "           O  | O   "
+echo "              |_    "
+echo "                    "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+sleep 0.2
+
+else
+
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo '           |       |'
+echo "          ==    == | "
+echo "           O  | O  "
+echo "              |_   "
+echo "                     "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+for (( x=1; x<=5; x++ ))
+do
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 1
+done
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo '           |       |'
+echo "          ==    == | "
+echo "           X  | X  "
+echo "              |_   "
+echo "                     "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 0.2
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo '           |       |'
+echo "          ==    == | "
+echo "           O  | O  "
+echo "              |_   "
+echo "                    "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+for ((x=1; x<=5; x++))
+do
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 1
+done
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo '           |       |'
+echo "          ==    == | "
+echo "           X  | X  "
+echo "              |_   "
+echo "                          "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 0.2
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "          ==    == |"
+echo "                   |"
+echo "           O  | O   "
+echo "              |_    "
+echo "                      "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 1
+
+{
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "                    "
+echo "           J ---  "
+echo "            /     \ "
+echo "          ==   ==  |"
+echo "                   |"
+echo "           X  | X   "
+echo "              |_    "
+echo "                           "
+echo "              ---   "
+echo "              ---   "
+echo "                           "
+echo "                           "
+ 
+} > jarbash.txt
+
+test -e /root/Recuerdos/tmp/pidtmp.txt && continue
+sleep 0.2
+
+fi
